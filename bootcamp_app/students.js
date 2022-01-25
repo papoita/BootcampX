167ebb4cd3fc550c556bb2b14bc830dd3fb1afa9
PaolaP <paolaperez8@gmail.com>
2022-01-24 22:33:12 -0500

    modify SQL to have a dif command line process.argv

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -10,15 +10,19 @@ const pool = new Pool({
 
 // writing SQL queries
 
+// const myArgs = process.argv.slice(2);
+// console.log('myArgs: ', myArgs);
+
 pool.query(`
-SELECT students.id as student_id, students.name as name, students.cohort_id as cohort_id, cohorts.name as cohort
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-JOIN cohorts ON cohort_id = cohorts.id
-LIMIT 5;
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
 `)
 .then(res => {
   res.rows.forEach(user=>{
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort_id} cohort named ${user.cohort}`);
+    console.log(`${user.name} has an id of ${user.student_id} and was in the cohort named ${user.cohort}`);
   });
 })
 .catch(err => console.error('query error', err.stack));
